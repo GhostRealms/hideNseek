6569aa181a49445f0c88002a30e0041549928d00
vtboy2000 <vtboy2000@hotmail.com>
2015-07-08 21:30:05 -0700

    basic structure with maven support

---
+++ src/main/java/net/ghostrealms/minigame/hindNseek.java
@@ -0,0 +1,13 @@
+package net.ghostrealms.minigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by James on 7/8/2015.
+ */
+public class hindNseek extends JavaPlugin{
+
+    public void onEnable(){
+        //TODO: see resources/todo.txt
+    }
+}
